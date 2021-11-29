44bf4ee5454b4f506ef9704dc9fa823889bbad37
OaCantona <duringjulian@gmail.com>
2021-11-29 21:11:07 +0100

    Improve margin on y axis

--- R/vis_transalp.R
+++ R/vis_transalp.R
@@ -4,7 +4,9 @@ vis_transalp <- function(gg_rides, gg_altitude) {
       title = "Transalp 2020",
       subtitle = "Albstadt - Lugano") &
     theme_pomological_fancy(base_size = 30) &
-    theme(legend.position = "none") &
+    theme(
+      legend.position = "none",
+      axis.title.y.right = element_text(margin = margin(l = 35))) &
     scale_color_manual(values = ggpomological:::pomological_palette[-4]) &
     scale_fill_manual(values = ggpomological:::pomological_palette[-4])
 }
