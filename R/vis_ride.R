3808820acfaebf22bc87b01ba9063ca460f7c5e0
OaCantona <duringjulian@gmail.com>
2021-11-29 22:41:53 +0100

    Visualise mountains

--- R/vis_ride.R
+++ R/vis_ride.R
@@ -8,5 +8,8 @@ vis_ride <- function(sf_act_meas, gg_alpen, df_poi) {
       mapping = aes(x = lng + offset, label = poi_name), alpha = 0.6,
       family = "Homemade Apple", size = 6.5, min.segment.length = 0,
       box.padding = 1.5) +
+    geom_point(
+      data = filter(df_poi, poi_type == "mountain"),
+      mapping = aes(x = lng), shape = 17) +
     labs(x = "Longitude", y = "Latitude")
 }
